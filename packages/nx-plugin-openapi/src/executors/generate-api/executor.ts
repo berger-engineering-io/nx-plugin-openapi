2656cbc02712266ed76b06eab725d80bedf92c85
Michael Berger <m.gonser@icloud.com>
2025-06-22 07:29:54 +0200

    feat(plugin): when remote url is given generate api from remote and calculate hash for caching

--- packages/nx-plugin-openapi/src/executors/generate-api/executor.ts
+++ packages/nx-plugin-openapi/src/executors/generate-api/executor.ts
@@ -1,8 +1,10 @@
 import { ExecutorContext, PromiseExecutor, logger } from '@nx/devkit';
 import { execSync } from 'child_process';
-import { rmSync } from 'fs';
+import { rmSync, mkdirSync, writeFileSync } from 'fs';
 import { join } from 'path';
 import { GenerateApiExecutorSchema } from './schema';
+import { RemoteCashedFileInfo } from './remote-cashed-file-info';
+import { createHash } from 'crypto';
 
 const runExecutor: PromiseExecutor<GenerateApiExecutorSchema> = async (
   options,
@@ -45,32 +47,9 @@ const runExecutor: PromiseExecutor<GenerateApiExecutorSchema> = async (
       stdio: 'inherit',
       cwd: context.root,
     });
+
     // currently disabled
-    if (false) {
-      if (inputSpec.startsWith('http://') || inputSpec.startsWith('https://')) {
-        console.log(`Spec path is a URL: ${inputSpec}`);
-        const response = await fetch(inputSpec);
-        if (response.ok) {
-          const content = await response.text();
-          const projectName = context.projectName || 'default';
-          // write the content to .nx-plugin-openapi/{projectName}/api.json
-          const apiPath = join(
-            context.root,
-            '.nx-plugin-openapi',
-            projectName,
-            'api.json'
-          );
-          const fs = require('fs');
-          fs.mkdirSync(join(context.root, '.nx-plugin-openapi', projectName), {
-            recursive: true,
-          });
-          fs.writeFileSync(apiPath, content);
-          console.log(`API spec saved to: ${apiPath}`);
-        }
-      } else {
-        console.log(`Spec path is a local file: ${inputSpec}`);
-      }
-    }
+    await handleRemote({ context, options });
 
     logger.info(
       `[@lambda-solutions/nx-plugin-openapi] API generation completed successfully.`
@@ -90,3 +69,88 @@ const runExecutor: PromiseExecutor<GenerateApiExecutorSchema> = async (
 };
 
 export default runExecutor;
+
+async function handleRemote(args: {
+  context: ExecutorContext;
+  options: GenerateApiExecutorSchema;
+}) {
+  /**
+   * If the inputSpec is a URL, fetch the content and save it to .nx-plugin-openapi/{projectName}/api.json
+   *
+   * Calculate hash and timestamp
+   */
+  const { context, options } = args;
+  const { inputSpec } = options;
+  if (inputSpec.startsWith('http://') || inputSpec.startsWith('https://')) {
+    logger.verbose(
+      `[@lambda-solutions/nx-plugin-openapi] Spec path is a remote URL: ${inputSpec}`
+    );
+    logger.verbose(
+      `[@lambda-solutions/nx-plugin-openapi] Fetching remote OpenAPI spec...`
+    );
+    const response = await fetch(inputSpec);
+    if (!response.ok) {
+      logger.error(
+        `[@lambda-solutions/nx-plugin-openapi] Failed to fetch remote OpenAPI spec: ${response.statusText}`
+      );
+    }
+    logger.verbose(
+      `[@lambda-solutions/nx-plugin-openapi] Remote OpenAPI spec fetched successfully.`
+    );
+    if (response.ok) {
+      const content = await response.text();
+      const projectName = context.projectName || 'default';
+      // write the content to .nx-plugin-openapi/{projectName}/api.json
+      const apiPath = join(
+        context.root,
+        // todo config option
+        '.nx-plugin-openapi',
+        projectName,
+        'api.json'
+      );
+
+      mkdirSync(join(context.root, '.nx-plugin-openapi', projectName), {
+        recursive: true,
+      });
+      writeFileSync(apiPath, content);
+
+      const cashedFilInfo = getRemoteCashedFileInfo({
+        fileContent: content,
+        remoteUrl: inputSpec,
+      });
+
+      const cashedFileInfoPath = join(
+        context.root,
+        '.nx-plugin-openapi',
+        projectName,
+        'cache-info.json'
+      );
+      writeFileSync(cashedFileInfoPath, JSON.stringify(cashedFilInfo, null, 2));
+
+      logger.verbose(
+        `[@lambda-solutions/nx-plugin-openapi] Remote API spec saved to: ${apiPath}`
+      );
+    }
+  } else {
+    logger.verbose(
+      `[@lambda-solutions/nx-plugin-openapi] Spec path is a local file: ${inputSpec}`
+    );
+  }
+}
+
+function getRemoteCashedFileInfo(args: {
+  fileContent: string;
+  remoteUrl: string;
+}): RemoteCashedFileInfo {
+  return {
+    hash: hashFileContent(args.fileContent),
+    timestamp: Date.now(),
+    remoteUrl: args.remoteUrl,
+  };
+}
+
+function hashFileContent(fileContent: string): string {
+  const hash = createHash('sha256');
+  hash.update(fileContent, 'utf8');
+  return hash.digest('hex');
+}
