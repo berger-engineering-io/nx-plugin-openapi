4611aaf103b62afbfe604854473ebb561f55c1dc
Michael Berger <m.gonser@icloud.com>
2025-09-11 09:48:14 +0200

    fix(core): do not install openapitools-generator-cli when init generator is executed

--- packages/core/src/generators/init/generator.ts
+++ packages/core/src/generators/init/generator.ts
@@ -1,16 +1,14 @@
 import {
-  addDependenciesToPackageJson,
   formatFiles,
+  GeneratorCallback,
   logger,
   readNxJson,
   runTasksInSerial,
   Tree,
   updateNxJson,
-  GeneratorCallback,
 } from '@nx/devkit';
 import { InitGeneratorSchema } from './schema';
 import { log } from '../utils/log';
-import { getPackageVersion } from '../utils/check-package-version';
 
 export async function initGenerator(tree: Tree, options: InitGeneratorSchema) {
   const packageJsonPath = 'package.json';
@@ -23,42 +21,7 @@ export async function initGenerator(tree: Tree, options: InitGeneratorSchema) {
     return Promise.resolve();
   }
 
-  const packageJson = JSON.parse(tree.read(packageJsonPath, 'utf-8'));
-  const dependencies = packageJson.dependencies || {};
-  const devDependencies = packageJson.devDependencies || {};
   const tasks: GeneratorCallback[] = [];
-  const packageName = '@openapitools/openapi-generator-cli';
-  const openApiGeneratorCliVersion =
-    (dependencies && dependencies[packageName]) ||
-    (devDependencies && devDependencies[packageName]);
-  if (!openApiGeneratorCliVersion) {
-    tasks.push(
-      addDependenciesToPackageJson(
-        tree,
-        {},
-        { '@openapitools/openapi-generator-cli': '^2.20.2' }
-      )
-    );
-  }
-
-  if (openApiGeneratorCliVersion) {
-    const version = getPackageVersion({
-      [packageName]: openApiGeneratorCliVersion,
-    });
-    if (version.major < 2) {
-      logger.warn(
-        log(
-          `You are using @openapitools/openapi-generator-cli version ${openApiGeneratorCliVersion}. It is recommended to use version 2.0.0 or above.`
-        )
-      );
-    } else {
-      logger.info(
-        log(
-          `Using @openapitools/openapi-generator-cli version ${openApiGeneratorCliVersion} which is compatible with this plugin.`
-        )
-      );
-    }
-  }
 
   updateTargetDefaults(tree);
 
